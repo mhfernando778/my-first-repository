72de4428e213c50ef6a8dcf328fa1c220825e72c
Fernando <mhfernando778@gmail.com>
2025-01-25 14:11:46 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -1,7 +1,11 @@
-// Task 1
-let employeeName = "Person Name";
+// Task 1: Employee Details //
+let employeeName = "Hoe Down";
 const employeeID = 12345;
 var isActive = true;
 
 console.log(employeeName);
 console.log(typeof employeeName);
+console.log(employeeID);
+console.log(typeof employeeID);
+console.log(isActive);
+Console.log(typeof isActive);
